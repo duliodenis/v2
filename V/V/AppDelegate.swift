aada58ed35070c6aecc7ada6a2525b259fd1bbfa
Dulio Denis <dulio.denis@yahoo.com>
2017-01-23 14:08:25 -0500

    Added Firebase via CocoaPods

--- V/V/AppDelegate.swift
+++ V/V/AppDelegate.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        FIRApp.configure()
+        
+        return true
+    }
+    
 }
 
